f54e753a5f2fa0c99d7e47ff5e704142c6300683
Nathan Yu <nathanyu@Nathans-MacBook-Air.local>
2022-04-26 00:43:46 -0400

    added test

---
+++ test/TestAdoption.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+    // The address of the adoption contract to be tested
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+    // The id of the pet that will be used for testing
+    uint256 expectedPetId = 0;
+
+    // The expected owner of adopted pet is this contract
+    address expectedAdopter = address(this);
+
+    function testUserCanAddPet() public {
+        adoption.addPet("Doge", "Shiba", 10, "Osaka, Japan");
+        uint size = adoption.getPetsLen();
+        Assert.equal(
+            size,
+            1,
+            "Addition of the expected pet should match what is returned."
+        );
+
+    }
+
+    function testUserCanAdoptPet() public {
+        uint256 returnedId = adoption.adopt(expectedPetId);
+
+        Assert.equal(
+            returnedId,
+            expectedPetId,
+            "Adoption of the expected pet should match what is returned."
+        );
+    }
+}
